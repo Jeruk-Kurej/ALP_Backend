9529b77b9464b19f106f4ae2bc5d8ca615273f07
Bryan Carlie Lukito Setiawan <blukitosetiawan@gmail.com>
2025-12-09 12:49:45 +0700

    Table, Login Register Feature, Setup Files Done

---
+++ src/service/user-service.ts
@@ -0,0 +1,71 @@
+import { ResponseError } from "../error/response-error"
+import {
+    LoginUserRequest,
+    RegisterUserRequest,
+    toUserResponse,
+    UserResponse,
+} from "../model/user-model"
+import { prismaClient } from "../util/database-util"
+import { UserValidation } from "../validation/user-validation"
+import { Validation } from "../validation/validation"
+import bcrypt from "bcrypt"
+
+export class UserService {
+    static async register(request: RegisterUserRequest): Promise<UserResponse> {
+        const validatedData = Validation.validate(
+            UserValidation.REGISTER,
+            request
+        )
+
+        const email = await prismaClient.user.findFirst({
+            where: {
+                email: validatedData.email,
+            },
+        })
+
+        if (email) {
+            throw new ResponseError(400, "Email has already existed!")
+        }
+
+        validatedData.password = await bcrypt.hash(validatedData.password, 12)
+
+        const user = await prismaClient.user.create({
+            data: {
+                username: validatedData.username,
+                email: validatedData.email,
+                password: validatedData.password,
+            },
+        })
+
+        return toUserResponse(user.id, user.username, user.email)
+    }
+
+    static async login(request: LoginUserRequest): Promise<UserResponse> {
+        const validatedData = Validation.validate(UserValidation.LOGIN, request)
+
+        const user = await prismaClient.user.findFirst({
+            where: {
+                email: validatedData.email,
+            },
+        })
+
+        if (!user) {
+            throw new ResponseError(400, "Invalid email or password!")
+        }
+
+        if (!user.is_active) {
+            throw new ResponseError(403, "Account is inactive!")
+        }
+
+        const passwordIsValid = await bcrypt.compare(
+            validatedData.password,
+            user.password
+        )
+
+        if (!passwordIsValid) {
+            throw new ResponseError(400, "Invalid email or password!")
+        }
+
+        return toUserResponse(user.id, user.username, user.email)
+    }
+}
---
+++ src/validation/user-validation.ts
@@ -0,0 +1,55 @@
+import { z, ZodType } from "zod"
+
+export class UserValidation {
+    static readonly REGISTER: ZodType = z.object({
+        username: z
+            .string({
+                error: "Username must be string!",
+            })
+            .min(1, {
+                error: "Username can not be empty!",
+            })
+            .max(150),
+        email: z
+            .email({
+                error: "Email format is invalid!",
+            })
+            .min(1, {
+                error: "Email can not be empty!",
+            })
+            .max(150),
+        password: z
+            .string({
+                error: "Password must be string!",
+            })
+            .min(8, {
+                error: "Password must contain at least 8 characters!",
+            })
+            .regex(/[A-Z]/, {
+                message: "Password must contain at least one uppercase letter"
+            })
+            .regex(/[a-z]/, {
+                message: "Password must contain at least one lowercase letter"
+            })
+            .regex(/[0-9]/, {
+                message: "Password must contain at least one number"
+            }),
+    })
+
+    static readonly LOGIN: ZodType = z.object({
+        email: z
+            .email({
+                error: "Email format is invalid!",
+            })
+            .min(1, {
+                error: "Email can not be empty!",
+            }),
+        password: z
+            .string({
+                error: "Password must be string!",
+            })
+            .min(1, {
+                error: "Password can not be empty!",
+            }),
+    })
+}
