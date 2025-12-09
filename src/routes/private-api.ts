f4ee1b482465a138faee0c3cf31edc470e5bcb04
Sean Lawton <slawton01@student.ciputra.ac.id>
2025-12-09 22:32:24 +0700

    Revert "Add product CRUD endpoints and validation"
    This reverts commit a57586096be4d25a7c56650ae9742c695186cbe6.

--- src/routes/private-api.ts
+++ src/routes/private-api.ts
@@ -1,12 +1,10 @@
 import express from "express"
 import { authMiddleware } from "../middleware/auth-middleware"
 import uploadRouter from "./upload-api"
-import productRouter from "./product-api"
 
 export const privateRouter = express.Router()
 
 privateRouter.use(authMiddleware)
 
 privateRouter.use(uploadRouter)
-privateRouter.use(productRouter)
 
